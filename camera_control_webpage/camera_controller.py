9a0d7c3d2a8e3272eebc1709e0d89ebd387b4840
Avichal Rakesh <avichal.rakesh@gmail.com>
2024-03-25 23:32:55 -0700

    show calibration rectangle on camera control webpage

--- camera_control_webpage/camera_controller.py
+++ camera_control_webpage/camera_controller.py
@@ -1,4 +1,7 @@
+from scipy.interpolate import CubicSpline
+from turbojpeg import TurboJPEG, TJFLAG_FASTUPSAMPLE, TJFLAG_FASTDCT
 from v4l2py.device import Device, BufferType
+import numpy as np
 import os
 import sys
 
@@ -27,6 +30,33 @@ class CameraController:
         self._cam.set_fps(BufferType.VIDEO_CAPTURE, 30)
         self._cam.controls.auto_exposure.value = 1
         self._cam.controls.white_balance_automatic.value = False
+
+        controlPoints = user_pref.read_calibration_data()
+        topX = [c[0] for c in controlPoints["top"]]
+        topY = [c[1] for c in controlPoints["top"]]
+        topSpline = CubicSpline(topX, topY)
+        self._topXs = [x for x in range(topX[0], topX[-1])]
+        self._topYs = np.rint(topSpline(self._topXs)).astype(np.int32)
+
+        bottomX = [c[0] for c in controlPoints["bottom"]]
+        bottomY = [c[1] for c in controlPoints["bottom"]]
+        bottomSpline = CubicSpline(bottomX, bottomY)
+        self._bottomXs = [x for x in range(bottomX[0], bottomX[-1])]
+        self._bottomYs = np.rint(bottomSpline(self._bottomXs)).astype(np.int32)
+
+        leftX = [c[0] for c in controlPoints["left"]]
+        leftY = [c[1] for c in controlPoints["left"]]
+        leftSpline = CubicSpline(leftY, leftX)
+        self._leftYs = [y for y in range(leftY[0], leftY[-1])]
+        self._leftXs = np.rint(leftSpline(self._leftYs)).astype(np.int32)
+
+        rightX = [c[0] for c in controlPoints["right"]]
+        rightY = [c[1] for c in controlPoints["right"]]
+        rightSpline = CubicSpline(rightY, rightX)
+        self._rightYs = [y for y in range(rightY[0], rightY[-1])]
+        self._rightXs = np.rint(rightSpline(self._rightYs)).astype(np.int32)
+
+        self._jpegDecoder = TurboJPEG()
 
 
     def get_control_bounds(self):
@@ -142,5 +172,13 @@ class CameraController:
     def stream_camera_frames(self):
         for frame in self._cam:
             yield (b"--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n"
-                   + bytes(frame)
+                   + self._draw_calibration_points_to_frame(frame)
                    + b"\r\n")
+
+    def _draw_calibration_points_to_frame(self, jpegFrame):
+        frame = self._jpegDecoder.decode(bytes(jpegFrame), flags=TJFLAG_FASTUPSAMPLE|TJFLAG_FASTDCT)
+        frame[self._topYs, self._topXs] = [0, 0, 255]
+        frame[self._bottomYs, self._bottomXs] = [0, 0, 255]
+        frame[self._leftYs, self._leftXs] = [0, 0, 255]
+        frame[self._rightYs, self._rightXs] = [0, 0, 255]
+        return self._jpegDecoder.encode(frame, quality=100)
