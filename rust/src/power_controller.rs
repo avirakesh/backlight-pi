35b0c1c6b87c86a0d73083752c6c03e42be81b19
Avichal Rakesh <avichal.rakesh@gmail.com>
2024-04-16 00:20:30 -0700

    rust: add comments to power_controller.rs

--- rust/src/power_controller.rs
+++ rust/src/power_controller.rs
@@ -9,18 +9,28 @@ use std::{
 
 use rppal::gpio::{Gpio, Level, Trigger};
 
+/**
+ * Monitors the power_pin for HIGH and LOW values, updates power_on,
+ * and unparks thread_handles when the value of power_on changes.
+ */
 pub(crate) fn monitor_power(
     power_pin: u8,
     power_on: Arc<AtomicBool>,
     thread_handles: Vec<JoinHandle<()>>,
 ) {
+    // Set up the GPIO Pin as input
     let mut input_pin = Gpio::new()
         .unwrap()
         .get(power_pin as u8)
         .unwrap()
         .into_input();
+    // Set an interrupt for both HIGH and LOW.
+    // LOW = Power OFF
+    // HIGH = POWER ON
     input_pin.set_interrupt(Trigger::Both).unwrap();
 
+    // Read the initial value so the various threads can start out in
+    // a consistent state.
     let read = input_pin.read();
     power_on.store(
         match read {
@@ -29,12 +39,15 @@ pub(crate) fn monitor_power(
         },
         Ordering::Relaxed,
     );
-    // unpark all threads just in case they need to come up
+
+    // unpark all threads just in case they need to come up.
     for handle in &thread_handles {
         handle.thread().unpark();
     }
 
     loop {
+        // Poll with 10s timeout to prevent the thread for being de-prioritized
+        // too much. Not sure if this actually helps :/
         let poll_result = input_pin.poll_interrupt(false, Option::Some(Duration::from_secs(10)));
         if poll_result.is_err() {
             panic!("Failed to poll for interrupt.");
@@ -42,6 +55,7 @@ pub(crate) fn monitor_power(
 
         let level_opt = poll_result.unwrap();
         if level_opt.is_none() {
+            // no value means the poll timed out.
             continue;
         }
 
